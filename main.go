beb32535e1a4394257dcaea46f34c9a3a599ea41
teracura <abdallahelbhary39@gmail.com>
2026-02-02 22:22:03 +0200

    improve api to have add instead of append

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"NotaborEngine/notamath"
 	"NotaborEngine/notashader"
 	"NotaborEngine/notassets"
-	"fmt"
 	"log"
 )
 
@@ -15,7 +14,6 @@ func main() {
 	engine := &notacore.Engine{
 		Settings: &notacore.Settings{Vsync: true},
 	}
-
 	if err := engine.InitPlatform(); err != nil {
 		log.Fatal("Init failed:", err)
 	}
@@ -35,7 +33,6 @@ func main() {
 		RenderLoop: renderLoop,
 		LogicLoops: []*notacore.FixedHzLoop{logicLoop},
 	}
-
 	win, err := engine.CreateWindow2D(cfg)
 	if err != nil {
 		panic(err)
@@ -55,19 +52,16 @@ func main() {
 	if err := win.CreateShader(texturedShader); err != nil {
 		log.Fatal("Failed to create shader:", err)
 	}
-
 	if err := win.UseShader("textured"); err != nil {
 		log.Fatal("Failed to use shader:", err)
 	}
 
 	entity := notassets.NewEntity("quad", "Test Quad")
-
 	sprite := &notassets.Sprite{
 		Texture: texture,
 		Name:    "quadSprite",
 		Polygon: notagl.CreateTextureQuad(1, 1),
 	}
-
 	rect := notagl.CreateRectangle(1, 1)
 	rect.SetColor(notashader.Yellow)
 	entity.SetSprite(sprite)
@@ -85,55 +79,35 @@ func main() {
 	rect2 := notagl.CreateRectangle(0.2, 2)
 	rect2.SetColor(notashader.Red)
 	entity2.SetPolygon(rect2)
-	entity2.SetCollider(notacollision.NewPolygonCollider(rect1))
+	entity2.SetCollider(notacollision.NewPolygonCollider(rect2))
 	entity2.Move(notamath.Vec2{X: -1, Y: 0})
 
-	// draw entity
-	renderLoop.Runnables = []notacore.Runnable{
-		func() error {
-			entity.Draw(win.RunTime.Renderer)
-			return nil
-		},
-	}
-
-	renderLoop.Runnables = append(renderLoop.Runnables,
-		func() error {
-			entity1.Draw(win.RunTime.Renderer)
-			return nil
-		})
-
-	renderLoop.Runnables = append(renderLoop.Runnables,
-		func() error {
-			entity2.Draw(win.RunTime.Renderer)
-			return nil
-		})
+	renderLoop.Add(func() error {
+		entity.Draw(win.RunTime.Renderer)
+		return nil
+	})
+	renderLoop.Add(func() error {
+		entity1.Draw(win.RunTime.Renderer)
+		return nil
+	})
+	renderLoop.Add(func() error {
+		entity2.Draw(win.RunTime.Renderer)
+		return nil
+	})
 
 	direction := 1.0
 
-	// rotate and move entity
-	logicLoop.Runnables = []notacore.Runnable{
-		func() error {
-			entity.Rotate(float32(direction * 0.01))
-			entity.Move(notamath.Vec2{
-				X: 0.001,
-				Y: 0,
-			}.Mul(float32(direction)))
-			return nil
-		},
-	}
-
-	logicLoop.Runnables = append(logicLoop.Runnables,
-		func() error {
-			if entity.CollidesWith(entity1) {
-				fmt.Println("Entity Collides with Entity")
-				direction *= -1.0
-			}
-			if entity.CollidesWith(entity2) {
-				fmt.Println("Entity Collides with Entity")
-				direction *= -1.0
-			}
-			return nil
-		})
+	logicLoop.Add(func() error {
+		entity.Rotate(float32(direction * 0.01))
+		entity.Move(notamath.Vec2{X: 0.001}.Mul(float32(direction)))
+		return nil
+	})
+	logicLoop.Add(func() error {
+		if entity.CollidesWith(entity1) || entity.CollidesWith(entity2) {
+			direction *= -1.0
+		}
+		return nil
+	})
 
 	if err := engine.Run(); err != nil {
 		log.Fatal("Engine run failed:", err)
